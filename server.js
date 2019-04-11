37f0e47b70cd571531c6db26c2607a8a6523469e
a-zuckut <zuckutalex@yahoo.com>
2019-04-11 10:39:19 -0400

    Added comments for server.js

--- server.js
+++ server.js
@@ -1,20 +1,18 @@
 var fs = require('fs');
 var http = require('http');
 var https = require('https');
-
 var express = require('express');
 var bodyParser = require('body-parser');
 
+// Initializing Express Server
 var app = express();
-var port = process.env.PORT || 8080;
+var port = process.env.PORT || 8080; // env port for heroku
 
-// your express configuration here
+// Start server
 var server = http.createServer(app);
 server.listen(port, listen);
 
-// help from: https://zellwk.com/blog/crud-express-mongodb/
-
-// This call back just tells us that the server has started
+// This call back gives confirmation server started
 function listen() {
   var host = server.address().address;
   var port = server.address().port;
@@ -22,15 +20,16 @@ function listen() {
   console.log('Example app listening at http://' + 'localhost' + ':' + port);
 }
 
-// DATABASE SETUP INFORMATION
+// setup MongoDB
 var MongoClient = require('mongodb').MongoClient;
 // The url that allows access to database
 var url = 'mongodb://testing:dbpasswordtest@cluster0-shard-00-00-afdih.mongodb.net:27017,cluster0-shard-00-01-afdih.mongodb.net:27017,cluster0-shard-00-02-afdih.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true';
 var db;
-// Cluster information for MongoDB
+// Cluster information for database (in future dependency injection could make more secure)
 var database_name = "database";
 var collection_name = "project";
 
+// Initializing MongoDB
 MongoClient.connect(url, function(err, client) {
   console.log("Connected to MongoDB Database");
   db = client.db(database_name);
@@ -38,53 +37,56 @@ MongoClient.connect(url, function(err, client) {
 
 app.use(bodyParser.json()); // for parsing application/json
 
+// Redirecting to index.html as default page
 app.get('/', function (req, res) {
   res.redirect('/index.html');
 });
 
+// API #1, savegrid will save to database and either update or save new item
 app.post("/game.html/savegrid", (req, res) => {
     console.log("Save Grid Post Request");
     var collection = db.collection(collection_name);
     if (req.body.new) {
-      console.log("Saving New Item")
+      console.log("Saving New Item.");
       collection.save(req.body.data, (err, result) => {
         if (err) return console.log(err);
-        console.log("saved successfully to database");
-        console.log("saveid: " + req.body.data.name);
+        console.log("New Grid ID: " + req.body.data.name);
       });
       res.end();
     } else {
-      console.log("update userid: " + req.body.user + ":" + req.body.userid)
+      console.log("Update User ID: " + req.body.user + ";" + req.body.userid);
       var myquery = {"name": req.body.data.name};
       var index = "data.grids." + req.body.userid;
       var newvalues = {$set: { [index]: req.body.data.data.grids[req.body.userid]} };
       collection.updateOne(myquery, newvalues, function(err, result) {
         if (err) return console.log(err);
-        console.log("Updating saveid: " + req.body.data.name);
+        console.log("Updating Grid ID: " + req.body.data.name);
       });
       res.end();
     }
 });
 
+// API #2, get will find a grid based on a Save ID
 app.get("/game.html/get", (req, res) => {
   console.log("Getting database data")
-  var find = req.query.project
+  var find = req.query.project // Save ID
   var cursor = db.collection(collection_name).find({"name": find}).next(function(err,items){
-    res.json(items)
+    res.json(items); // return via json
   })
 });
 
+// API #3, get all users and return these through a list
 app.get("/getusers", (req, res) => {
   // get users as list from database
-  list = [];
+  list = []; // return value
   db.collection(collection_name).find().forEach((data) => {
     for (var i = 0; i < data.data.grids.length; i++) {
+      // go through all grids and if an user exists for a grid, add it to the list
       if (data.data.grids[i].user != null)
-        list.push(data.data.grids[i].user)
+        list.push(data.data.grids[i].user);
     }
   }).then(function(count) {
-    console.log(list)
-    res.json(list)
+    res.json(list);
   });
 });
 
